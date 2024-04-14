2fb22ae3dc3a9add03c8ab4ee723f4687752c772
mukulms123 <mukulsingh.ms123@gmail.com>
2024-04-14 16:31:09 +0530

    Video 28 - User Model created

---
+++ backend/models/user.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+      maxLength: [50, "Your name cannot exceed 50 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minLength: [8, "Password must be bigger than 8 characters"],
+      select: false,
+    },
+    avatar: {
+      public_id: {
+        type: String,
+      },
+      url: {
+        type: String,
+      },
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
