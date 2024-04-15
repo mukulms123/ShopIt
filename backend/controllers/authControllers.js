f732947eecab88cdb7ed5bcdee740e4a6cc08337
mukulms123 <mukulsingh.ms123@gmail.com>
2024-04-15 20:51:11 +0530

    Video 30 - JWT token added, JWT token generated with registration

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,7 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import User from "../models/user.js";
 
+//Register user => /api/v1/register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await User.create({
@@ -9,7 +10,10 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
     password,
   });
 
+  const token = user.getJwtToken();
+
   res.status(201).json({
     success: "user created",
+    token,
   });
 });
