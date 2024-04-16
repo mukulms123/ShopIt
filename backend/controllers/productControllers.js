16ff58a36595660cee8af451e59a131e7fa2d63d
mukulms123 <mukulsingh.ms123@gmail.com>
2024-04-16 21:00:04 +0530

    Video 36 - User id added to products while product creation

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -26,6 +26,8 @@ export const getProducts = catchAsyncErrors(async (req, res) => {
 
 // Create new products => /api/v1/admin/products
 export const newProducts = catchAsyncErrors(async (req, res) => {
+  req.body.user = req.user._id;
+
   const product = await Product.create(req.body);
 
   res.status(200).json({ product });
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -10,12 +10,16 @@ import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+router.route("/products").get(getProducts);
 router
-  .route("/products")
-  .get(isAuthenticatedUser, authorizeRoles("admin"), getProducts);
-router.route("/admin/products").post(newProducts);
+  .route("/admin/products")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), newProducts);
 router.route("/products/:id").get(getProductDetails);
-router.route("/admin/products/:id").put(updateProductById);
-router.route("/admin/products/:id").delete(deleteProductById);
+router
+  .route("/admin/products/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProductById);
+router
+  .route("/admin/products/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProductById);
 
 export default router;
