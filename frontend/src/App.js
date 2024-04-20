613d48c400b8de1e425cb843192081b9cb7fa7eb
mukulms123 <mukulsingh.ms123@gmail.com>
2024-04-20 17:01:23 +0530

    Video 57 - Added Header, Footer, css,

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,14 @@
+import "./App.css";
+
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Shop-It</h1>
+      <Footer />
     </div>
   );
 }
