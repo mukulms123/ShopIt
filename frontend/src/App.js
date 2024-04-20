31a5d23bd70034938cb0f908e22661a64d556504
mukulms123 <mukulsingh.ms123@gmail.com>
2024-04-20 17:32:21 +0530

    Video 59 - added React-Router-Dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 
 import Footer from "./components/layout/Footer";
@@ -6,13 +7,17 @@ import Header from "./components/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div>
-        <Home />
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
