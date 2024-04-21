60771c62b28a1eec8a942ffdcc61a2a153977839
mukulms123 <mukulsingh.ms123@gmail.com>
2024-04-21 13:08:43 +0530

    Video 66 - Toaster added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MetaData from "./layout/MetaData";
 import { useGetProductsQuery } from "../redux/api/productsApi";
 import ProductItem from "./product/ProductItem";
 import Loader from "./layout/Loader";
+import toast from "react-hot-toast";
 
 const Home = () => {
-  const { data, isLoading } = useGetProductsQuery();
+  const { data, isLoading, error, isError } = useGetProductsQuery();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(error?.data?.message);
+    }
+  }, [isError]);
 
   if (isLoading) return <Loader />;
   return (
